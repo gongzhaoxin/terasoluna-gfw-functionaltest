4fbcc0f59ad49b3c75e7d77cf7735f0cb273f087
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2020-04-15 13:49:01 +0900

    log contextpath

--- terasoluna-gfw-functionaltest-web/src/main/java/org/terasoluna/gfw/functionaltest/app/welcome/WelcomeController.java
+++ terasoluna-gfw-functionaltest-web/src/main/java/org/terasoluna/gfw/functionaltest/app/welcome/WelcomeController.java
@@ -15,19 +15,27 @@
  */
 package org.terasoluna.gfw.functionaltest.app.welcome;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
 
+    private static final Logger logger = LoggerFactory
+            .getLogger(WelcomeController.class);
+
     @RequestMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        logger.info("Request URI  is " + request.getRequestURI());
+        logger.info("Context Root is " + request.getContextPath());
+        logger.info("Servlet Path is " + request.getServletPath());
+        logger.info("Path Info    is " + request.getPathInfo());
         return "welcome/index";
     }
 
-    @RequestMapping("/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    public String aaaaa() {
-        return "welcome/index";
-    }
 }
